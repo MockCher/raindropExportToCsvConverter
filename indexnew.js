263fba0e293fb33feeb7976cd8ff9af94a89d88a
Bastian <mockcher@posteo.de>
2020-08-03 21:59:16 +0200

    removed files

--- indexnew.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-function readRDHtml(file){
-    // var html = '';
-    return fs.readFileSync(file, 'utf8');
-}
-
-var html = readRDHtml('Raindrop.html');
-console.log(html);
